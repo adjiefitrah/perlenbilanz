7c73f25040e2115a757d0fa3d7dcea9186e7f6a9
Jörn Friedrich Dreyer <jfd@butonic.de>
2014-01-03 12:00:06 +0100

    relayout overview

--- templates/partials/uebersicht.php
+++ templates/partials/uebersicht.php
@@ -1,10 +1,10 @@
 <div id="uebersicht">
-	<h1>Übersicht aktueller Monat</h1>
+	<h1>Übersicht</h1>
 	<form>
-	<div class="centered">
-		<div>
-
-			<table style="float: left; width: 300px; margin-right: -300px;" class="overview">
+	<div>
+		<div style="float: left; text-align: left; width: 300px; margin-right: -300px; height: 120px;">
+			<h2>Aktueller Monat</h2>
+			<table style="width: 300px;" class="overview">
 				<tr>
 					<th></th>
 					<th>Brutto</th>
@@ -30,297 +30,296 @@
 					<td ng-show="( now | date:'yyyy') < 2014" style="text-align: right;">{{vkNetto-ekNetto|number_de}} €</td>
 				</tr>
 			</table>
-			<div style="float:right; width:100%;">
-				<div style="margin-left: 330px;padding-right: 20px;">
-					<textarea class="notes" ng-model="notes" ng-change="saveNotes()"></textarea>
+			<br/>
+			<h2>Notizen</h2>
+			<div style="width: 285px; height: 300px;">
+				<textarea class="notes" ng-model="notes" ng-change="saveNotes()"></textarea>
+			</div>
+		</div>
+		<div>
+			<div style="margin-left: 330px;">
+
+				<div class="uebersicht">
+
+					<h2>Verkäufe</h2>
+
+					<table id="verkaeufe">
+						<tr>
+							<th>Id</th>
+							<th>Plattform</th>
+							<th>Account</th>
+							<th>Name</th>
+							<th>Brutto</th>
+							<th ng-show="( now | date:'yyyy') < 2014">MwSt</th>
+							<th ng-show="( now | date:'yyyy') < 2014">Netto</th>
+							<th>Zahlweise</th>
+							<th></th>
+						</tr>
+						<tr ng-repeat="verkauf in verkaeufe" ng-click="editVerkauf(verkauf.id)">
+
+							<td class="id">{{verkauf.id}}</td>
+
+							<td class="plattform">{{verkauf.plattform}}</td>
+
+							<td class="account">{{verkauf.account}}</td>
+
+							<td class="name">{{verkauf.name}}</td>
+
+							<td class="brutto">{{verkauf.brutto|number_de}} €</td>
+
+							<td ng-show="( now | date:'yyyy') < 2014" class="mwst">{{verkauf.mwst|number_de}} €</td>
+
+							<td ng-show="( now | date:'yyyy') < 2014" class="netto">{{verkauf.netto|number_de}} €</td>
+
+							<td class="zahlweise">{{verkauf.zahlweise}}</td>
+
+							<td class="status">
+								<div class="icon gezahlt"
+									 title="Gezahlt"
+									 ng-hide="verkauf.wertstellung|empty"></div>
+								<div class="icon placeholder" ng-show="verkauf.wertstellung|empty"></div>
+
+								<div class="icon rechnung"
+									 title="Rechnung"
+									 ng-hide="verkauf.rechnungsnummer|empty"></div>
+								<div class="icon placeholder" ng-show="verkauf.rechnungsnummer|empty"></div>
+
+								<div class="icon placeholder" ng-show="verkauf.geliefert == 0"></div>
+								<div class="icon versand_teilweise"
+									 title="Teilweise Versand"
+									 ng-show="verkauf.geliefert > 0 && verkauf.geliefert < 1"></div>
+								<div class="icon versand"
+									 title="Versand"
+									 ng-show="verkauf.geliefert == 1"></div>
+							</td>
+
+						</tr>
+
+
+						<tr class="sum" ng-show="verkaeufe.length > 0">
+
+							<td class="id"></td>
+
+							<td class="plattform"></td>
+
+							<td class="account"></td>
+
+							<td class="name" style="text-align: right;">Summe:</td>
+
+							<td class="brutto">{{calcBruttoSum(verkaeufe)|number_de}} €</td>
+
+							<td ng-show="( now | date:'yyyy') < 2014" class="brutto">{{calcMwStSum(verkaeufe)|number_de}} €</td>
+
+							<td ng-show="( now | date:'yyyy') < 2014" class="brutto">{{calcNettoSum(verkaeufe)|number_de}} €</td>
+
+							<td class="zahlweise"></td>
+
+							<td class="status"></td>
+
+						</tr>
+
+					</table>
+				</div>
+
+				<br/>
+
+				<div class="uebersicht" ng-show="overdueVerkaeufe.length > 0">
+
+					<h2>Überfällige Verkäufe aus Vormonaten</h2>
+
+					<table id="verkaeufe" class="overdue">
+						<tr>
+							<th>Id</th>
+							<th>Plattform</th>
+							<th>Account</th>
+							<th>Name</th>
+							<th>Brutto</th>
+							<th ng-show="( now | date:'yyyy') < 2014">MwSt</th>
+							<th ng-show="( now | date:'yyyy') < 2014">Netto</th>
+							<th>Zahlweise</th>
+							<th></th>
+						</tr>
+						<tr ng-repeat="verkauf in overdueVerkaeufe" ng-click="editVerkauf(verkauf.id)">
+
+							<td class="id">{{verkauf.id}}</td>
+
+							<td class="plattform">{{verkauf.plattform}}</td>
+
+							<td class="account">{{verkauf.account}}</td>
+
+							<td class="name">{{verkauf.name}}</td>
+
+							<td class="brutto">{{verkauf.brutto|number_de}} €</td>
+
+							<td ng-show="( now | date:'yyyy') < 2014" class="mwst">{{verkauf.mwst|number_de}} €</td>
+
+							<td ng-show="( now | date:'yyyy') < 2014" class="netto">{{verkauf.netto|number_de}} €</td>
+
+							<td class="zahlweise">{{verkauf.zahlweise}}</td>
+
+							<td class="status">
+								<div class="icon gezahlt"
+									 title="Wertstellung"
+									 ng-hide="verkauf.wertstellung|empty"></div>
+								<div class="icon placeholder" ng-show="verkauf.wertstellung|empty"></div>
+
+								<div class="icon rechnung"
+									 title="Beleg"
+									 ng-hide="verkauf.rechnungsnummer|empty"></div>
+								<div class="icon placeholder" ng-show="verkauf.rechnungsnummer|empty"></div>
+
+								<div class="icon placeholder" ng-show="verkauf.geliefert == 0"></div>
+								<div class="icon versand_teilweise"
+									 title="Teilweise Versand"
+									 ng-show="verkauf.geliefert > 0 && verkauf.geliefert < 1"></div>
+								<div class="icon versand"
+									 title="Versand"
+									 ng-show="verkauf.geliefert == 1"></div>
+							</td>
+
+						</tr>
+
+					</table>
+				</div>
+
+				<br/>
+
+				<div class="uebersicht">
+
+					<h2>Einkäufe</h2>
+
+					<table id="einkaeufe">
+						<tr>
+							<th>Id</th>
+							<th>Plattform</th>
+							<th>Account</th>
+							<th>Name</th>
+							<th>Brutto</th>
+							<th ng-show="( now | date:'yyyy') < 2014">MwSt</th>
+							<th ng-show="( now | date:'yyyy') < 2014">Netto</th>
+							<th>Zahlweise</th>
+							<th></th>
+						</tr>
+						<tr ng-repeat="einkauf in einkaeufe" ng-click="editEinkauf(einkauf.id)">
+
+							<td class="id">{{einkauf.id}}</td>
+
+							<td class="plattform">{{einkauf.plattform}}</td>
+
+							<td class="account">{{einkauf.account}}</td>
+
+							<td class="name">{{einkauf.name}}</td>
+
+							<td class="brutto">{{einkauf.brutto|number_de}} €</td>
+
+							<td ng-show="( now | date:'yyyy') < 2014" class="mwst">{{einkauf.mwst|number_de}} €</td>
+
+							<td ng-show="( now | date:'yyyy') < 2014" class="netto">{{einkauf.netto|number_de}} €</td>
+
+							<td class="zahlweise">{{einkauf.zahlweise}}</td>
+
+							<td class="status">
+								<div class="icon gezahlt"
+									 title="Wertstellung"
+									 ng-hide="einkauf.wertstellung|empty"></div>
+								<div class="icon placeholder"
+									 ng-show="einkauf.wertstellung|empty"></div>
+
+								<div class="icon placeholder" ng-show="einkauf.geliefert == 0"></div>
+								<div class="icon versand_teilweise"
+									 title="Teilweise Versand"
+									 ng-show="einkauf.geliefert > 0 && einkauf.geliefert < 1"></div>
+								<div class="icon versand"
+									 title="Versand"
+									 ng-show="einkauf.geliefert == 1"></div>
+							</td>
+
+						</tr>
+
+						<tr class="sum" ng-show="einkaeufe.length > 0">
+
+							<td class="id"></td>
+
+							<td class="plattform"></td>
+
+							<td class="account"></td>
+
+							<td class="name" style="text-align: right;">Summe:</td>
+
+							<td class="brutto">{{calcBruttoSum(einkaeufe)|number_de}} €</td>
+
+							<td ng-show="( now | date:'yyyy') < 2014" class="mwst">{{calcMwStSum(einkaeufe)|number_de}} €</td>
+
+							<td ng-show="( now | date:'yyyy') < 2014" class="netto">{{calcNettoSum(einkaeufe)|number_de}} €</td>
+
+							<td class="zahlweise"></td>
+
+							<td class="status"></td>
+
+						</tr>
+
+					</table>
+				</div>
+
+				<br/>
+
+				<div class="uebersicht" ng-show="overdueEinkaeufe.length > 0">
+
+					<h2>Überfällige Einkäufe aus Vormonaten</h2>
+
+					<table id="einkaeufe" class="overdue">
+						<tr>
+							<th>Id</th>
+							<th>Plattform</th>
+							<th>Account</th>
+							<th>Name</th>
+							<th>Brutto</th>
+							<th ng-show="( now | date:'yyyy') < 2014">MwSt</th>
+							<th ng-show="( now | date:'yyyy') < 2014">Netto</th>
+							<th>Zahlweise</th>
+							<th></th>
+						</tr>
+						<tr ng-repeat="einkauf in overdueEinkaeufe" ng-click="editEinkauf(einkauf.id)">
+
+							<td class="id">{{einkauf.id}}</td>
+
+							<td class="plattform">{{einkauf.plattform}}</td>
+
+							<td class="account">{{einkauf.account}}</td>
+
+							<td class="name">{{einkauf.name}}</td>
+
+							<td class="brutto">{{einkauf.brutto|number_de}} €</td>
+
+							<td ng-show="( now | date:'yyyy') < 2014" class="mwst">{{einkauf.mwst|number_de}} €</td>
+
+							<td ng-show="( now | date:'yyyy') < 2014" class="netto">{{einkauf.netto|number_de}} €</td>
+
+							<td class="zahlweise">{{einkauf.zahlweise}}</td>
+
+							<td class="status">
+								<div class="icon gezahlt"
+									 title="Gezahlt"
+									 ng-hide="einkauf.wertstellung|empty"></div>
+								<div class="icon placeholder"
+									 ng-show="einkauf.wertstellung|empty"></div>
+
+								<div class="icon placeholder" ng-show="einkauf.geliefert == 0"></div>
+								<div class="icon versand_teilweise"
+									 title="Teilweise Versand"
+									 ng-show="einkauf.geliefert > 0 && einkauf.geliefert < 1"></div>
+								<div class="icon versand"
+									 title="Versand"
+									 ng-show="einkauf.geliefert == 1"></div>
+							</td>
+
+						</tr>
+
+					</table>
+				</div>
+		
 				</div>
 			</div>
 		</div>
-		
-		<hr/>
-
-		<div class="uebersicht">
-		
-			<h2>Verkäufe</h2>
-			
-			<table id="verkaeufe">
-				<tr>
-					<th>Id</th>
-					<th>Plattform</th>
-					<th>Account</th>
-					<th>Name</th>
-					<th>Brutto</th>
-					<th ng-show="( now | date:'yyyy') < 2014">MwSt</th>
-					<th ng-show="( now | date:'yyyy') < 2014">Netto</th>
-					<th>Zahlweise</th>
-					<th></th>
-				</tr>
-				<tr ng-repeat="verkauf in verkaeufe" ng-click="editVerkauf(verkauf.id)">
-
-					<td class="id">{{verkauf.id}}</td>
-
-					<td class="plattform">{{verkauf.plattform}}</td>
-
-					<td class="account">{{verkauf.account}}</td>
-
-					<td class="name">{{verkauf.name}}</td>
-
-					<td class="brutto">{{verkauf.brutto|number_de}} €</td>
-
-					<td ng-show="( now | date:'yyyy') < 2014" class="mwst">{{verkauf.mwst|number_de}} €</td>
-
-					<td ng-show="( now | date:'yyyy') < 2014" class="netto">{{verkauf.netto|number_de}} €</td>
-
-					<td class="zahlweise">{{verkauf.zahlweise}}</td>
-
-					<td class="status">
-						<div class="icon gezahlt"
-						     title="Gezahlt"
-						     ng-hide="verkauf.wertstellung|empty"></div>
-						<div class="icon placeholder" ng-show="verkauf.wertstellung|empty"></div>
-						
-						<div class="icon rechnung"
-						     title="Rechnung"
-						     ng-hide="verkauf.rechnungsnummer|empty"></div>
-						<div class="icon placeholder" ng-show="verkauf.rechnungsnummer|empty"></div>
-						
-						<div class="icon placeholder" ng-show="verkauf.geliefert == 0"></div>
-						<div class="icon versand_teilweise"
-						     title="Teilweise Versand"
-						     ng-show="verkauf.geliefert > 0 && verkauf.geliefert < 1"></div>
-						<div class="icon versand"
-						     title="Versand"
-						     ng-show="verkauf.geliefert == 1"></div>
-					</td>
-					
-				</tr>
-
-				
-				<tr class="sum" ng-show="verkaeufe.length > 0">
-
-					<td class="id"></td>
-
-					<td class="plattform"></td>
-
-					<td class="account"></td>
-
-					<td class="name" style="text-align: right;">Summe:</td>
-
-					<td class="brutto">{{calcBruttoSum(verkaeufe)|number_de}} €</td>
-					
-					<td ng-show="( now | date:'yyyy') < 2014" class="brutto">{{calcMwStSum(verkaeufe)|number_de}} €</td>
-					
-					<td ng-show="( now | date:'yyyy') < 2014" class="brutto">{{calcNettoSum(verkaeufe)|number_de}} €</td>
-
-					<td class="zahlweise"></td>
-					
-					<td class="status"></td>
-
-				</tr>
-				
-			</table>
-		</div>
-		
-		<br/>
-
-		<div class="uebersicht" ng-show="overdueVerkaeufe.length > 0">
-		
-			<h2>Überfällige Verkäufe aus Vormonaten</h2>
-			
-			<table id="verkaeufe" class="overdue">
-				<tr>
-					<th>Id</th>
-					<th>Plattform</th>
-					<th>Account</th>
-					<th>Name</th>
-					<th>Brutto</th>
-					<th ng-show="( now | date:'yyyy') < 2014">MwSt</th>
-					<th ng-show="( now | date:'yyyy') < 2014">Netto</th>
-					<th>Zahlweise</th>
-					<th></th>
-				</tr>
-				<tr ng-repeat="verkauf in overdueVerkaeufe" ng-click="editVerkauf(verkauf.id)">
-
-					<td class="id">{{verkauf.id}}</td>
-
-					<td class="plattform">{{verkauf.plattform}}</td>
-
-					<td class="account">{{verkauf.account}}</td>
-
-					<td class="name">{{verkauf.name}}</td>
-
-					<td class="brutto">{{verkauf.brutto|number_de}} €</td>
-
-					<td ng-show="( now | date:'yyyy') < 2014" class="mwst">{{verkauf.mwst|number_de}} €</td>
-
-					<td ng-show="( now | date:'yyyy') < 2014" class="netto">{{verkauf.netto|number_de}} €</td>
-
-					<td class="zahlweise">{{verkauf.zahlweise}}</td>
-
-					<td class="status">
-						<div class="icon gezahlt"
-						     title="Wertstellung"
-						     ng-hide="verkauf.wertstellung|empty"></div>
-						<div class="icon placeholder" ng-show="verkauf.wertstellung|empty"></div>
-						
-						<div class="icon rechnung"
-						     title="Beleg"
-						     ng-hide="verkauf.rechnungsnummer|empty"></div>
-						<div class="icon placeholder" ng-show="verkauf.rechnungsnummer|empty"></div>
-						
-						<div class="icon placeholder" ng-show="verkauf.geliefert == 0"></div>
-						<div class="icon versand_teilweise"
-						     title="Teilweise Versand"
-						     ng-show="verkauf.geliefert > 0 && verkauf.geliefert < 1"></div>
-						<div class="icon versand"
-						     title="Versand"
-						     ng-show="verkauf.geliefert == 1"></div>
-					</td>
-					
-				</tr>
-				
-			</table>
-		</div>
-		
-		<hr/>
-		
-		<div class="uebersicht">
-		
-			<h2>Einkäufe</h2>
-			
-			<table id="einkaeufe">
-				<tr>
-					<th>Id</th>
-					<th>Plattform</th>
-					<th>Account</th>
-					<th>Name</th>
-					<th>Brutto</th>
-					<th ng-show="( now | date:'yyyy') < 2014">MwSt</th>
-					<th ng-show="( now | date:'yyyy') < 2014">Netto</th>
-					<th>Zahlweise</th>
-					<th></th>
-				</tr>
-				<tr ng-repeat="einkauf in einkaeufe" ng-click="editEinkauf(einkauf.id)">
-
-					<td class="id">{{einkauf.id}}</td>
-
-					<td class="plattform">{{einkauf.plattform}}</td>
-
-					<td class="account">{{einkauf.account}}</td>
-
-					<td class="name">{{einkauf.name}}</td>
-
-					<td class="brutto">{{einkauf.brutto|number_de}} €</td>
-
-					<td ng-show="( now | date:'yyyy') < 2014" class="mwst">{{einkauf.mwst|number_de}} €</td>
-
-					<td ng-show="( now | date:'yyyy') < 2014" class="netto">{{einkauf.netto|number_de}} €</td>
-
-					<td class="zahlweise">{{einkauf.zahlweise}}</td>
-
-					<td class="status">
-						<div class="icon gezahlt"
-						     title="Wertstellung"
-						     ng-hide="einkauf.wertstellung|empty"></div>
-						<div class="icon placeholder"
-						     ng-show="einkauf.wertstellung|empty"></div>
-						
-						<div class="icon placeholder" ng-show="einkauf.geliefert == 0"></div>
-						<div class="icon versand_teilweise"
-						     title="Teilweise Versand"
-						     ng-show="einkauf.geliefert > 0 && einkauf.geliefert < 1"></div>
-						<div class="icon versand"
-						     title="Versand"
-						     ng-show="einkauf.geliefert == 1"></div>
-					</td>
-
-				</tr>
-
-				<tr class="sum" ng-show="einkaeufe.length > 0">
-
-					<td class="id"></td>
-
-					<td class="plattform"></td>
-
-					<td class="account"></td>
-
-					<td class="name" style="text-align: right;">Summe:</td>
-
-					<td class="brutto">{{calcBruttoSum(einkaeufe)|number_de}} €</td>
-
-					<td ng-show="( now | date:'yyyy') < 2014" class="mwst">{{calcMwStSum(einkaeufe)|number_de}} €</td>
-
-					<td ng-show="( now | date:'yyyy') < 2014" class="netto">{{calcNettoSum(einkaeufe)|number_de}} €</td>
-
-					<td class="zahlweise"></td>
-
-					<td class="status"></td>
-
-				</tr>
-				
-			</table>
-		</div>
-		
-		<br/>
-		
-		<div class="uebersicht" ng-show="overdueEinkaeufe.length > 0">
-		
-			<h2>Überfällige Einkäufe aus Vormonaten</h2>
-			
-			<table id="einkaeufe" class="overdue">
-				<tr>
-					<th>Id</th>
-					<th>Plattform</th>
-					<th>Account</th>
-					<th>Name</th>
-					<th>Brutto</th>
-					<th ng-show="( now | date:'yyyy') < 2014">MwSt</th>
-					<th ng-show="( now | date:'yyyy') < 2014">Netto</th>
-					<th>Zahlweise</th>
-					<th></th>
-				</tr>
-				<tr ng-repeat="einkauf in overdueEinkaeufe" ng-click="editEinkauf(einkauf.id)">
-
-					<td class="id">{{einkauf.id}}</td>
-
-					<td class="plattform">{{einkauf.plattform}}</td>
-
-					<td class="account">{{einkauf.account}}</td>
-
-					<td class="name">{{einkauf.name}}</td>
-
-					<td class="brutto">{{einkauf.brutto|number_de}} €</td>
-
-					<td ng-show="( now | date:'yyyy') < 2014" class="mwst">{{einkauf.mwst|number_de}} €</td>
-
-					<td ng-show="( now | date:'yyyy') < 2014" class="netto">{{einkauf.netto|number_de}} €</td>
-
-					<td class="zahlweise">{{einkauf.zahlweise}}</td>
-
-					<td class="status">
-						<div class="icon gezahlt"
-						     title="Gezahlt"
-						     ng-hide="einkauf.wertstellung|empty"></div>
-						<div class="icon placeholder"
-						     ng-show="einkauf.wertstellung|empty"></div>
-						
-						<div class="icon placeholder" ng-show="einkauf.geliefert == 0"></div>
-						<div class="icon versand_teilweise"
-						     title="Teilweise Versand"
-						     ng-show="einkauf.geliefert > 0 && einkauf.geliefert < 1"></div>
-						<div class="icon versand"
-						     title="Versand"
-						     ng-show="einkauf.geliefert == 1"></div>
-					</td>
-
-				</tr>
-
-			</table>
-		</div>
-		
-	</div>
 	</form>
 </div>
-
-
-
